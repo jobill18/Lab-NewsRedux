81713498febfadce2446bed74af364b4634ed12e
Joseph Billings <josephbillings.gb@gmail.com>
2024-07-08 13:29:28 -0600

    created redditReducer and connected it to the store and the Reddit.jsx page to display the articles

--- src/components/Reddit/Reddit.jsx
+++ src/components/Reddit/Reddit.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
-import Card from '../shared/Card/Card.jsx';
-import Loading from '../shared/Loading/Loading.jsx';
+import { useSelector, useDispatch } from "react-redux";
+import { requestArticles } from "../../reducers/redditReducer.js";
+import { useEffect } from "react";
+import Card from "../shared/Card/Card.jsx";
+import Loading from "../shared/Loading/Loading.jsx";
 
 export default function Reddit() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const articleCards = articles.map((article) => <Card key={article.id} article={article} />);
+  const articles = useSelector((state) => state.reddit.articles);
+  const loading = useSelector((state) => state.reddit.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(requestArticles);
+  }, []);
+
+  const articleCards = articles.map((article) => (
+    <Card key={article.id} article={article} />
+  ));
 
   return (
     <div className="news-container">
-      <img src="../../assets/redditLogo.png" alt="" className='logo' />
+      <img src="../../assets/redditLogo.png" alt="" className="logo" />
       {loading ? <Loading /> : <div>{articleCards}</div>}
     </div>
   );
