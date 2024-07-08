18caa868ca0520c54629ceccea7594bc59d71cf4
Joseph Billings <josephbillings.gb@gmail.com>
2024-07-08 12:16:37 -0600

    first commit

---
+++ src/reducers/hackerNewsReducer.js
@@ -0,0 +1,8 @@
+const initialState = { loading: false, article: [] };
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
