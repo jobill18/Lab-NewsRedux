fc7af4013b792cc1ec2b3c4aad47373320f80d52
Joseph Billings <josephbillings.gb@gmail.com>
2024-07-08 13:05:10 -0600

    used async function in HackerNews.jsx to display articles

--- src/components/HackerNews/HackerNews.jsx
+++ src/components/HackerNews/HackerNews.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
-import Card from '../shared/Card/Card.jsx';
-import Loading from '../shared/Loading/Loading.jsx';
+import { useSelector, useDispatch } from "react-redux";
+import { requestArticles } from "../../reducers/hackerNewsReducer.js";
+import { useEffect } from "react";
+import { useState } from "react";
+import Card from "../shared/Card/Card.jsx";
+import Loading from "../shared/Loading/Loading.jsx";
 
 export default function HackerNews() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const articles = useSelector((state) => state.articles);
+  const loading = useSelector((state) => state.loading);
+  const dispatch = useDispatch();
 
-  const articleCards = articles.map((article) => <Card key={article.id} article={article} />);
+  useEffect(() => {
+    dispatch(requestArticles);
+  }, []);
+
+  const articleCards = articles.map((article) => (
+    <Card key={article.id} article={article} />
+  ));
   return (
     <div className="news-container">
-      <img className='logo' src="../../assets/hackerNews.jpeg" alt="" />
+      <img className="logo" src="../../assets/hackerNews.jpeg" alt="" />
       {loading ? <Loading /> : <div>{articleCards}</div>}
     </div>
   );
